a75340ddf7f141096148f290756e2ef0af794d5d
JimCode <jimmypaytan10@gmail.com>
2022-07-11 10:21:02 -0500

    crud destinos

--- src/components/destinos/DestinosTabla.js
+++ src/components/destinos/DestinosTabla.js
@@ -1,9 +1,27 @@
 import imgHeader1 from "../../assets/img/header1.jpg";
+import './DestinosTabla.css'
 import {useEffect, useState} from "react";
+import {Box, Button, Modal} from "@mui/material";
 
+const estilosModal = {
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 400,
+    bgcolor: 'background.paper',
+    border: '2px solid #000',
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+};
 
 function DestinosTabla() {
     const [destinos, setDestinos] = useState([]);
+    const [paisDestino, setPaisDestino] = useState('');
+    const [descripcionDestino, setDescripcionDestino] = useState('');
+    const [modalRegistro, setModalRegistro] = useState(false);
 
     useEffect(() => {
         getDestinos()
@@ -19,6 +37,72 @@ function DestinosTabla() {
                 setDestinos(data)
             })
     }
+
+    const registrarDestinos = (event) => {
+        event.preventDefault();
+        cerrarModalRegistro()
+        let ruta = `http://api-alonso.test/api/destinos`
+        const formData = new FormData()
+        formData.append('pais', paisDestino)
+        formData.append('descripcion', descripcionDestino)
+        fetch(ruta, {
+            method: 'POST',
+            body: formData
+        }).then(response => {
+            return response.json();
+        }).then(data => {
+            getDestinos()
+            alert("Se ha registrado los destinos " + nombre + " de manera correcta.")
+
+        })
+
+    }
+
+    const abrirModelRegistro = () => {
+        setModalRegistro(true)
+    }
+
+    const cerrarModalRegistro = () => {
+        setModalRegistro(false)
+    }
+
+    const formularioTemplate = () => {
+        return (
+            <>
+                <div className="add_comment">
+                    <div className="_title">Complete los siguientes campos:</div>
+                    <div className="field">
+                        <label htmlFor="pais" className="label">Pais</label>
+                        <div className="input_wrap">
+                            <input
+                                type="text"
+                                id="pais"
+                                className="input"
+                                value={paisDestino}
+                                onChange={(e) => setPaisDestino(e.target.value)}/>
+                        </div>
+                    </div>
+                    <div className="field">
+                        <label htmlFor="descripcion" className="label">Descripción</label>
+                        <div className="input_wrap">
+                            <textarea
+                                id="descripcion"
+                                className="textarea"
+                                value={descripcionDestino}
+                                onChange={(e) => setDescripcionDestino(e.target.value)}
+                            />
+                        </div>
+                    </div>
+
+                    <div className="comment-fotm-bottom">
+                        <button className="btn submit">Enviar</button>
+                        <Button onClick={cerrarModalRegistro}>Cancelar</Button>
+                    </div>
+                </div>
+            </>
+        )
+    }
+
     return (
         <>
             <div className="breadcrumbs">
@@ -38,6 +122,9 @@ function DestinosTabla() {
                     <div className="wrap_float">
                         <div className="section-subtitle">EXPLORA LOS TOURS POR DESTINO</div>
                         <div className="section-title">DESTINOS</div>
+                        <div className="section-title">
+                            <button className="btn-agregar-destino" onClick={abrirModelRegistro}>Añadir destino</button>
+                        </div>
                         <div className="main">
                             <div className="popular_destination__slider">
                                 <table className="table table-striped slide_item" style={{
@@ -57,14 +144,15 @@ function DestinosTabla() {
                                     {
                                         destinos.map(destino =>
                                             <tr key={destino.id}>
-                                                <th scope="row">{ destino.id }</th>
-                                                <td>{ destino.pais }</td>
-                                                <td>{ destino.descripcion }</td>
+                                                <th scope="row">{destino.id}</th>
+                                                <td>{destino.pais}</td>
+                                                <td>{destino.descripcion}</td>
                                                 <td>
-                                                    <img src={ destino.img_bandera } alt=""/>
+                                                    <img src={destino.img_bandera} alt=""/>
                                                 </td>
                                                 <td>
-                                                    <button className="btn btn__choose_tour">Editar</button>
+                                                    <button className="btn-editar-destino">Editar</button>
+                                                    <button className="btn-eliminar-destino">Eliminar</button>
                                                 </td>
                                             </tr>
                                         )
@@ -77,6 +165,26 @@ function DestinosTabla() {
                     </div>
                 </div>
             </div>
+
+            <Modal
+                hideBackdrop
+                open={modalRegistro}
+                onClose={cerrarModalRegistro}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+            >
+                <Box sx={{...estilosModal, width: '50%'}}>
+                    <h2 id="child-modal-title">REGISTRAR DESTINO</h2>
+                    <div id="child-modal-description">
+                        <div className="reviews_comments">
+                            <form>
+                                {formularioTemplate()}
+                            </form>
+                        </div>
+                    </div>
+
+                </Box>
+            </Modal>
         </>
     )
 }
